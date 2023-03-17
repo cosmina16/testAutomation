be4bc49e2a3ae9b4094a7d62b969d6772b6ef353
cosmina16 <cosminabahna@gmail.com>
2023-03-17 18:33:05 +0200

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //prima data am adaugat depedinte=proiecte/module deja scrise pe care noi le vom folosi in proiectul nostru
+            //testNG - modul cu care putem scrie teste automate sau unit test
+                //vine cu adnotari- clase deja scrise pe care le putem lega unor metode altor clase atirbbute ect.
+                    //ex. beforeclass - exexuta setup-ul ala inainte de a executa orice din acea clasa
+            //boni garcia - web driver manager cu care putem scrie teste pt fiecare browser
+                        //nu trebuie sa ai versiunea de la cel care primesti proiectul
+            //selenium - folosit in testare automata pt a apasa butoane
+
     }
 }
--- src/test/java/Test/GoogleTest.java
+++ src/test/java/Test/GoogleTest.java
@@ -10,21 +10,31 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
+    //WebDriver - de la selenium si
+    // face interactiunea cu web-ul cu pagina
     WebDriver driver;
 
     @BeforeClass
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();//se executa inainte de fiecare clasa
+        //fac set-ul unui browser(chrome)-setez existenta unui driver de chrome
+        //exista un driver atunci cand noi dam click
     }
 
     @Test
     public void SearchGoogleTest() {
+        //instantiz driver-ul
         driver = new ChromeDriver();
         driver.get("https://google.com");
 
+        //butoanele = WebElement asa le vede selenium
+        //By -e o clasa(sau se poate dupa CSS selesct)
+
+        //iau butonul in variabila acc..
         WebElement acceptCookiesButton = driver.findElement(By.id("L2AGLb"));
         acceptCookiesButton.click();
 
+        //scriem un stir de caractere(ce vrem sa cautam) si ce tasta vrem sa apasam
         WebElement searchInput = driver.findElement(By.name("q"));
         searchInput.sendKeys("Google Atelierul Digital", Keys.ENTER);
     }
